7a769b14ad1edb40e85f3c4b0457866f634ad09b
GlennBenavides <glenn.benavides.28.agosto@gmail.com>
2015-08-11 11:11:27 -0600

    Clase Credit (4)

--- EasyBanking/src/java/com/easybanking/banking/Credit.java
+++ EasyBanking/src/java/com/easybanking/banking/Credit.java
@@ -17,8 +17,18 @@ public class Credit extends BankAccount implements Interesable {
     private final double minimumMonthPayment = 100;
     private double loanLastBalance;
     private double remainingCreditAmount;
+    private boolean hasPermissionToLoan;
+    
+    // Borrar los siguientes comentarios después de leer:
+    // NO cambiar los finals..!!  
+    // loanLastBalance guarda el saldo de la deuda, lo que todavìa se debe
+    // remainingCreditAmount guarda el saldo de lìmite de crèdito, o saldo usable
+    // la booleana guarda TRUE si la persona està habilitada para usar el crèdito,
+    // si el cliente deja de hacer uno de sus pagos mensuales obligatorios,
+    // esta booleana debe ponerse en FALSE
 
     public Credit() {
+        hasPermissionToLoan = true;
     }
 
     public Credit(double loanLastBalance, double remainingCreditAmount) {
@@ -41,7 +51,40 @@ public class Credit extends BankAccount implements Interesable {
     public void setRemainingCreditAmount(double remainingCreditAmount) {
         this.remainingCreditAmount = remainingCreditAmount;
     }
+
+    public boolean isHasPermissionToLoan() {
+        return hasPermissionToLoan;
+    }
+
+    public void setHasPermissionToLoan(boolean hasPermissionToLoan) {
+        this.hasPermissionToLoan = hasPermissionToLoan;
+    }
     
     
-    
-}
+        public void amortizationToLoan(double paymentAmount) {
+            
+            if (paymentAmount >= this.minimumMonthPayment && this.isHasPermissionToLoan() == true)) {
+                this.setLoanLastBalance(this.getLoanLastBalance() - paymentAmount);
+                this.setRemainingCreditAmount(this.getRemainingCreditAmount() + paymentAmount);
+            } else {
+                System.out.println("Exception");
+            }
+            
+        }
+        
+        
+        
+        
+      public void loanFromLoanBalance(double loanAmount){
+            
+            if (loanAmount >= this.getRemainingCreditAmount()) {
+                this.setLoanLastBalance(this.getLoanLastBalance() + loanAmount);
+                this.setRemainingCreditAmount(this.getRemainingCreditAmount() - loanAmount);
+            } else {
+                System.out.println("Exception");
+            }
+            
+        }
+            
+        }
+       
