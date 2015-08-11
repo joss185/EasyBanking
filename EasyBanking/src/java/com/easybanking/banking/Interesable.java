1c3184e6273b4407be54ed6b6b05420f04ad0047
GlennBenavides <glenn.benavides.28.agosto@gmail.com>
2015-08-11 11:35:03 -0600

    Glenn (2)

--- EasyBanking/src/java/com/easybanking/banking/Interesable.java
+++ EasyBanking/src/java/com/easybanking/banking/Interesable.java
@@ -11,6 +11,16 @@ package com.easybanking.banking;
  */
 public interface Interesable {
     
-    public double getInterest(); 
+    // ¿A ver? ¿INTERÉS es un monto con dos funciones: 1) monto que se 
+    // suma positivamente a las cuentas de los clientes como beneficio por
+    // tener dinero ahorrado... y 2) monto que se le cobra a los clientes
+    // de crédito cada vez que pagan su mensualidad y que se cobra como un
+    // extra a su mensualidad?
+    // El interés para el crédito es un porcentaje del saldo de la deuda..!!!!! ????
+    
+    // Si todo lo anterior es verdadero, podemos tener dos métodos para calcular el interest:
+    
+    public double getInterestRegularAccount(); 
+    public double getInterestCreditAccount();
     
 }
